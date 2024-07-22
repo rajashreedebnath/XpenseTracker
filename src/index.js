36f8d3ecb8f668942ce70c838cd0598fa266798a
Mohak-Trivedi <trivedimohak@gmail.com>
2024-07-23 00:24:32 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// DO NOT TOUCH THE BELOW LINE
+import reportWebVitals from "./reportWebVitals";
+
+// DO NOT TOUCH THE BELOW 3 LINES
+if (window.Cypress) {
+  window.store = store;
+}
+
+// WRITE YOUR CODE HERE
+
+// DO NOT TOUCH THE BELOW LINE
+reportWebVitals();
